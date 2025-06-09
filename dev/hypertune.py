d3e8770de1558f9cd04344a932b34251d3ca9083
raoul grouls <Raoul.Grouls@han.nl>
2025-06-09 21:19:00 +0200

    update hypertune

--- dev/hypertune.py
+++ dev/hypertune.py
@@ -1,29 +1,20 @@
 from pathlib import Path
 from typing import Dict
 
-import numpy as np
 import ray
 import torch
 from filelock import FileLock
 from loguru import logger
-from mltrainer import ReportTypes, Trainer, TrainerSettings, rnn_models
+from mltrainer import ReportTypes, Trainer, TrainerSettings, metrics, rnn_models
 from mltrainer.preprocessors import PaddedPreprocessor
 from ray import tune
 from ray.tune import CLIReporter
-from ray.tune.schedulers import AsyncHyperBandScheduler
 from ray.tune.search.hyperopt import HyperOptSearch
+from ray.tune.schedulers import AsyncHyperBandScheduler
 
-logger.remove()
-logger.add("logs/logfile.log", level="DEBUG")
+NUM_SAMPLES = 50
+MAX_EPOCHS = 10
 
-
-
-class Accuracy:
-    def __repr__(self) -> str:
-        return "Accuracy"
-
-    def __call__(self, y, yhat):
-        return (np.argmax(yhat, axis=1) == y).sum() / len(yhat)
 
 def train(config: Dict):
     """
@@ -33,7 +24,7 @@ def train(config: Dict):
     """
     from mads_datasets import DatasetFactoryProvider, DatasetType
 
-    data_dir = config["data_dir"]
+    data_dir = config.pop("data_dir")
     gesturesdatasetfactory = DatasetFactoryProvider.create_factory(DatasetType.GESTURES)
     preprocessor = PaddedPreprocessor()
 
@@ -48,11 +39,12 @@ def train(config: Dict):
 
     # we set up the metric
     # and create the model with the config
-    accuracy = Accuracy()
-    model = rnn_models.GRUmodel(config)
+    accuracy = metrics.Accuracy()
+    modelconfig = rnn_models.ModelConfig(**config)
+    model = rnn_models.GRUmodel(modelconfig)
 
     trainersettings = TrainerSettings(
-        epochs=50,
+        epochs=MAX_EPOCHS,
         metrics=[accuracy],
         logdir=Path("."),
         train_steps=len(train),  # type: ignore
@@ -70,6 +62,16 @@ def train(config: Dict):
     # This is why we set earlystop_kwargs=None, because we
     # are handing over this control to ray.
 
+    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = torch.device("mps")
+    else:
+        device = "cpu"  # type: ignore
+    logger.info(f"Using {device}")
+    if device != "cpu":
+        logger.warning(
+            f"using acceleration with {device}.Check if it actually speeds up!"
+        )
+
     trainer = Trainer(
         model=model,
         settings=trainersettings,
@@ -78,6 +80,7 @@ def train(config: Dict):
         traindataloader=train.stream(),
         validdataloader=valid.stream(),
         scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau,
+        device=str(device),
     )
 
     trainer.loop()
@@ -90,12 +93,18 @@ if __name__ == "__main__":
     if not data_dir.exists():
         data_dir.mkdir(parents=True)
         logger.info(f"Created {data_dir}")
-    tune_dir = Path("models/ray").resolve()
+    tune_dir = Path("logs/ray").resolve()
+    search = HyperOptSearch()
+    scheduler = AsyncHyperBandScheduler(
+        time_attr="training_iteration",
+        grace_period=1,
+        reduction_factor=3,
+        max_t=MAX_EPOCHS,
+    )
 
     config = {
         "input_size": 3,
         "output_size": 20,
-        "tune_dir": tune_dir,
         "data_dir": data_dir,
         "hidden_size": tune.randint(16, 128),
         "dropout": tune.uniform(0.0, 0.3),
@@ -105,22 +114,14 @@ if __name__ == "__main__":
     reporter = CLIReporter()
     reporter.add_metric_column("Accuracy")
 
-    search = HyperOptSearch()
-    scheduler = AsyncHyperBandScheduler(
-        time_attr="training_iteration",
-        grace_period=1,
-        reduction_factor=3,
-        max_t=50,
-    )
-
     analysis = tune.run(
         train,
         config=config,
         metric="test_loss",
         mode="min",
         progress_reporter=reporter,
-        storage_path=str(config["tune_dir"]),
-        num_samples=10,
+        storage_path=str(tune_dir),
+        num_samples=NUM_SAMPLES,
         search_alg=search,
         scheduler=scheduler,
         verbose=1,
